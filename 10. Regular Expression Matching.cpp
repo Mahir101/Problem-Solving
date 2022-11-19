f0a61a277981ea54a70df77c020312001b32c7e7
Md. Mahir Labib <mdmahirlabib@gmail.com>
2022-11-19 15:16:16 +0600

    Hard | Recursion with handled conditions

---
+++ 10. Regular Expression Matching.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool solve(int sPos, int pPos, vector<vector<bool>>& visited, string& s, string& p) {
+        if(sPos == -1) {
+            while(pPos>=0 && p[pPos]=='*') {
+                pPos -= 2;
+            }
+        }
+
+        if(sPos==-1 && pPos==-1) 
+            return true;
+        if(sPos==-1 || pPos==-1) 
+            return false;
+        if(visited[sPos][pPos]) 
+            return false;
+        
+        visited[sPos][pPos] = true;
+        
+        if(p[pPos]=='.') {
+            return solve(sPos-1, pPos-1, visited, s, p);
+        } else if(p[pPos] == '*') {
+            bool ret = false;
+            char prev = p[pPos-1];
+            if (prev=='.' || prev==s[sPos]) {
+                ret = solve(sPos-1, pPos, visited, s, p);
+            }
+            return ret || solve(sPos, pPos-2, visited, s, p);
+        } else {
+            return s[sPos]==p[pPos]? solve(sPos-1, pPos-1, visited, s, p):false;
+        }
+    }
+    
+    bool isMatch(string s, string p) {
+        int sizeOfS = s.size();
+        int sizeOfP = p.size();
+        vector<vector<bool>> visited(sizeOfS, vector<bool> (sizeOfP, false));
+        return solve(sizeOfS-1, sizeOfP-1, visited, s, p);
+    }
+};

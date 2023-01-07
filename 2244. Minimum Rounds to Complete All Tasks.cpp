a28c018c0dd69c2692477656e1cf00cb9c7aaf71
Md. Mahir Labib <mdmahirlabib@gmail.com>
2023-01-07 10:57:27 +0600

    Freq array impl

---
+++ 2244. Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<tasks.size();i++){
+            mp[tasks[i]]++;
+        }
+        
+        int ans=0;
+        
+        for (auto freq : mp) {
+            if(freq.second==1) {
+                return -1;
+            }
+            if(freq.second%3==0) {
+                ans += freq.second/3;
+            }
+            else {
+                ans += freq.second/3 + 1;
+            }
+        }
+        return ans;
+    }
+};

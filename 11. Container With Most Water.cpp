a8443789bb549c23f855aeca94149dd5d95e60a7
Md. Mahir Labib <mdmahirlabib@gmail.com>
2023-04-12 02:04:41 +0600

    2p

---
+++ 11. Container With Most Water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int ans = 0, n = height.size(), i = 0, j = n-1;
+       while(i<j){
+           if(height[i]>height[j]){
+               ans = max(ans, (j-i)*height[j]);
+               j--;
+           }
+           else{
+               ans = max(ans, (j-i)*height[i]);
+               i++;
+           }
+       }
+       return ans;
+    }
+};
